6b6cd426552124de7c5b791b5ed718f72146390c
mniksch <mattniksch@yahoo.com>
2017-10-16 23:33:48 -0500

    CS-92 Some progress towards native PDF

--- reports_modules/create_pdf.py
+++ reports_modules/create_pdf.py
@@ -1,16 +1,64 @@
 #!python3
+'''Module for making the PDF versions of the Single Student View tab
+   from the main report'''
 
 import warnings
+from datetime import date
+from collections import OrderedDict
 from fpdf import FPDF
+
+def compute_excel(string, data_row):
+    '''Takes a string encoded as an excel formula and returns a numeric
+    (float) response. Data from the student roster table is provided
+    in order to be replaced by Excel references'''
+    return None
+
+def clean_excel(string, data_row):
+    '''Takes a string encoded as an Excel formula and returns a plain
+    text string for pdf printing. Data from the student roster table
+    is provided in data_row and could be replace based on the an Excel
+    reference'''
+    # We need to do these in order so that the quotes at the end aren't
+    # replaced inside the formulas in the beginning
+    translation = OrderedDict([
+            ['TEXT(TODAY(),"mm/dd")', date.today().strftime('%m/%d')],
+            ['TEXT(TODAY(),"m/d")', date.today().strftime('%m/%d')],
+            ['INDEX(Counselor,MATCH(D3,KidIDs,0))','tbl:Counselor'],
+            ['INDEX(Advisors,MATCH(D3,KidIDs,0))','tbl:Advisor'],
+            ['INDEX(Cohort,MATCH(D3,KidIDs,0))','tbl:Cohort'],
+            ['&',''],
+            ['=',''],
+            ['"',''],
+            ])
+    for old, new in translation.items():
+        if old in string:
+            if new.startswith('tbl:'):
+                string = string.replace(old, data_row[new[4:]])
+            else:
+                string = string.replace(old, new)
+    return string
+
+def shrink_cell(pdf, w, txt, h, border, ln, align, fill):
+    ''' writes a cell, but cuts off last characters if too long'''
+    while w < pdf.get_string_width(txt):
+        txt = txt[:-1]
+    pdf.cell(w=w, txt=txt, h=h, border=border,
+            ln=ln, align=align, fill=fill)
 
 def make_pdf_report(fn, dfs, cfg, cfg_ssv, campus, debug):
     '''Master function for creating the pdf reports'''
 
     # First create Class and process config settings
-    if campus in cfg_ssv['pdf_orientation']:
-        orient = cfg_ssv['pdf_orientation'][campus]
-    else:
-        orient = cfg_ssv['pdf_orientation']['Standard']
+    local_cfg = {}
+    for label, ssv_name in [('orient', 'pdf_orientation'),
+                            ('c_header', 'counselor_header'),
+                            ('p_header', 'print_header'),
+                            ('p_footer', 'print_footer'),
+                            ]:
+        if campus in cfg_ssv[ssv_name]:
+            local_cfg[label] = cfg_ssv[ssv_name][campus]
+        else:
+            local_cfg[label] = cfg_ssv[ssv_name]['Standard']
 
     top_margin = cfg_ssv['pdf_margins']['top']
     left_margin = cfg_ssv['pdf_margins']['left']
@@ -18,97 +66,126 @@ def make_pdf_report(fn, dfs, cfg, cfg_ssv, campus, debug):
     thick_line = cfg_ssv['pdf_lines']['thick']
     line = cfg_ssv['pdf_lines']['line']
 
-    pdf = FPDF(orientation = orient, unit = 'in', format = 'Letter')
+    pdf = FPDF(orientation = local_cfg['orient'],
+            unit = 'in', format = 'Letter')
 
     for font_name, filename in cfg_ssv['pdf_fonts'].items():
         pdf.add_font(font_name, '', filename, uni=True)
     pdf.set_line_width(line)
     pdf.set_margins(left=left_margin, top=top_margin, right=right_margin)
 
-    pdf.add_page()
+    # Get the student data and sort as appropriate
+    df = dfs['roster'].copy()
+    if debug:
+        print(df.index)
+        print(df.columns)
+    ##NEED A SORT HERE
 
-    pdf.set_font('font_b', '', 14)
-    pdf.set_y(top_margin)
+    # start repeating here
+    for i, stu_data in df.iterrows():
+        pdf.add_page()
 
-    w = cfg_ssv['pdf_widths'] # list of cell widths in inches
-    h = cfg_ssv['pdf_heights'] # list of cell heights in inches
+        pdf.set_y(top_margin)
 
-    # The width of next two rows is variable if special header
-    # First row
-    pdf.cell(w=sum(w[:3]),
-            txt='College application odds report for Example Student',
-            h=h[0], border = 0, ln = 0, align = 'L', fill = False)
+        w = cfg_ssv['pdf_widths'] # list of cell widths in inches
+        h = cfg_ssv['pdf_heights'] # list of cell heights in inches
 
-    pdf.cell(w=w[3], txt='',
-            h=h[0], border = 0, ln = 0, align = 'L', fill = False)
+        # The width of next two columns is variable based on header sizes
+        # First row
+        name_text = ('College application odds report for '+
+                stu_data['First']+' '+stu_data['Last'])
+        c_text = clean_excel(local_cfg['c_header'],stu_data)
+        pdf.set_font('font_b', '', 11)
+        c_width = pdf.get_string_width(c_text)+0.05
 
-    pdf.set_font('font_b', '', 11)
-    pdf.cell(w=sum(w[4:]), txt='Cnslr: Ballard (B1)',
-            h=h[0], border = 0, ln = 1, align = 'L', fill = False)
+        if local_cfg['p_header']:
+            # We're squeezing in one more entry, so stealing off the name
+            # and the counselor header
+            pdf.set_font('font_i', '', 11)
+            p_text = clean_excel(local_cfg['p_header'], stu_data)
+            p_width = pdf.get_string_width(p_text)+0.05
+            n_width = sum(w) - p_width - c_width - 0.05
+        else:
+            n_width = sum(w) - c_width - 0.05
 
-    # Second row
-    pdf.set_fill_color(r=220,g=230,b=241)
-    pdf.cell(w=w[0], txt="Student's name:",
-            h=h[1], border = 1, ln = 0, align = 'L', fill = True)
+        pdf.set_font('font_b', '', 14)
+        shrink_cell(pdf=pdf, w=n_width, txt=name_text,
+                h=h[0], border = 0, ln = 0, align = 'L', fill = False)
 
-    pdf.set_font('font_r', '', 11)
-    pdf.cell(w=w[1], txt='ACT/SAT',
-            h=h[1], border = 'B', ln = 0, align = 'C', fill = True)
+        if local_cfg['p_header']:
+            pdf.set_font('font_i', '', 11)
+            shrink_cell(pdf=pdf, w=p_width, txt=p_text,
+                    h=h[0], border = 0, ln = 0, align = 'L', fill = False)
 
-    pdf.cell(w=w[2], txt='GPA',
-            h=h[1], border = 'B', ln = 0, align = 'C', fill = True)
+        pdf.set_font('font_b', '', 11)
+        shrink_cell(pdf=pdf, w=c_width, txt=c_text,
+                h=h[0], border = 0, ln = 1, align = 'L', fill = False)
 
-    pdf.cell(w=w[3], txt='Race/Eth',
-            h=h[1], border = 1, ln = 0, align = 'C', fill = True)
+        # Second row
+        pdf.set_fill_color(r=220,g=230,b=241)
+        pdf.cell(w=w[0], txt="Student's name:",
+                h=h[1], border = 1, ln = 0, align = 'L', fill = True)
 
-    pdf.cell(w=w[4], txt='IGR', # check for special names
-            h=h[1], border = 'B', ln = 0, align = 'C', fill = True)
+        pdf.set_font('font_r', '', 11)
+        pdf.cell(w=w[1], txt='ACT/SAT',
+                h=h[1], border = 'B', ln = 0, align = 'C', fill = True)
 
-    pdf.cell(w=w[5], txt='74%',
-            h=h[1], border = 'B', ln = 1, align = 'C', fill = False)
+        pdf.cell(w=w[2], txt='GPA',
+                h=h[1], border = 'B', ln = 0, align = 'C', fill = True)
 
-    # Third row
-    pdf.set_fill_color(r=253,g=233,b=217)
-    pdf.cell(w=w[0], txt='Last, First',
-            h=h[2], border = 1, ln = 0, align = 'L', fill = True)
+        pdf.cell(w=w[3], txt='Race/Eth',
+                h=h[1], border = 1, ln = 0, align = 'C', fill = True)
 
-    pdf.cell(w=w[1], txt='22/1120',
-            h=h[2], border = 0, ln = 0, align = 'C', fill = False)
+        pdf.cell(w=w[4], txt='IGR', # check for special names
+                h=h[1], border = 'B', ln = 0, align = 'C', fill = True)
 
-    pdf.cell(w=w[2], txt='3.57',
-            h=h[2], border = 0, ln = 0, align = 'C', fill = False)
+        pdf.cell(w=w[5], txt='74%',
+                h=h[1], border = 'B', ln = 1, align = 'C', fill = False)
 
-    pdf.cell(w=w[3], txt='H',
-            h=h[2], border = 1, ln = 0, align = 'C', fill = False)
+        # Third row
+        pdf.set_fill_color(r=253,g=233,b=217)
+        pdf.cell(w=w[0], txt='Last, First',
+                h=h[2], border = 1, ln = 0, align = 'L', fill = True)
 
-    pdf.set_fill_color(r=220,g=230,b=241)
-    pdf.cell(w=w[4], txt='TGR', # check for special names
-            h=h[2], border = 'T', ln = 0, align = 'C', fill = True)
+        pdf.cell(w=w[1], txt='22/1120',
+                h=h[2], border = 0, ln = 0, align = 'C', fill = False)
 
-    pdf.cell(w=w[5], txt='68%',
-            h=h[2], border = 0, ln = 1, align = 'C', fill = False)
+        pdf.cell(w=w[2], txt='3.57',
+                h=h[2], border = 0, ln = 0, align = 'C', fill = False)
 
-    # Fourth row
+        pdf.cell(w=w[3], txt='H',
+                h=h[2], border = 1, ln = 0, align = 'C', fill = False)
 
-    # Bold lines
-    pdf.set_line_width(thick_line)
-    pdf.rect(left_margin, top_margin+h[0], # around name
-            w[0], sum(h[1:3]))
+        pdf.set_fill_color(r=220,g=230,b=241)
+        pdf.cell(w=w[4], txt='TGR', # check for special names
+                h=h[2], border = 'T', ln = 0, align = 'C', fill = True)
 
-    pdf.rect(left_margin, top_margin+h[0], # around name and scores/gpa
-            sum(w[:3]), sum(h[1:3]))
+        pdf.cell(w=w[5], txt='68%',
+                h=h[2], border = 0, ln = 1, align = 'C', fill = False)
 
-    pdf.rect(left_margin, top_margin+h[0], # around whole left side
-            sum(w[:3]), sum(h[1:6]))
+        # Fourth row
 
-    pdf.rect(left_margin+sum(w[:4]), top_margin+h[0], # upper right
-            sum(w[4:]), sum(h[1:3]))
+        # Bold lines
+        pdf.set_line_width(thick_line)
+        pdf.rect(left_margin, top_margin+h[0], # around name
+                w[0], sum(h[1:3]))
 
-    pdf.rect(left_margin+sum(w[:3]), top_margin+sum(h[:3]), # lower right
-            sum(w[3:]), sum(h[3:6]))
+        pdf.rect(left_margin, top_margin+h[0], # around name and scores/gpa
+                sum(w[:3]), sum(h[1:3]))
 
-    pdf.line(left_margin,top_margin,left_margin+sum(w[:4]),top_margin)
-    pdf.set_line_width(line)
+        pdf.rect(left_margin, top_margin+h[0], # around whole left side
+                sum(w[:3]), sum(h[1:6]))
+
+        pdf.rect(left_margin+sum(w[:4]), top_margin+h[0], # upper right
+                sum(w[4:]), sum(h[1:3]))
+
+        pdf.rect(left_margin+sum(w[:3]), top_margin+sum(h[:3]), # lower right
+                sum(w[3:]), sum(h[3:6]))
+
+        pdf.line(left_margin,top_margin,left_margin+sum(w[:4]),top_margin)
+
+        # Skinny lines
+        pdf.set_line_width(line)
 
     # The font we use is missing an unusued glyph and so throws two warnings
     # at save. The next two lines supress this, but probably good to
--- run_all.py
+++ run_all.py
@@ -1,17 +1,28 @@
 #!python3
 """Runs all college-lists"""
 from time import time
-#import datetime
+import datetime
 import os
+import zipfile
 
-#dtfolder = datetime.datetime.now().strftime('%m_%d_%Y')
-#os.mkdir(dtfolder)
+def compress(filefolder):
+    '''Will replace a folder with a zip file of the same name
+       NOTE: will only work with a flat directory--does not nest'''
+    zipfn = os.path.basename(filefolder) + '.zip'
+    with zipfile.ZipFile(zipfn, 'w', zipfile.ZIP_DEFLATED) as myzip:
+        os.chdir(filefolder)
+        for file in os.listdir('.'):
+            print('Compressing %s.' % file)
+            myzip.write(file)
+            os.remove(file)
+    os.chdir('..')
+    os.rmdir(filefolder)
+
 call_stem = 'python create_reports.py'
-'''
+
 t0 = time()
 os.system(call_stem+' -sum Campus')
 print('Network: {:.2f} seconds'.format(time()-t0),flush=True)
-'''
 
 for campus_case in [
         'Noble',
@@ -33,51 +44,32 @@ for campus_case in [
         ]:
     t0 = time()
     print('Generating {}...'.format(campus_case),flush=True,end='')
-    os.system(call_stem+' -q -ca '+campus_case)
+    if campus_case == 'Johnson':
+        os.system(call_stem+' -s settings_aa_only.yaml -q -ca '+campus_case)
+    else:
+        os.system(call_stem+' -q -ca '+campus_case)
     print('{:.2f} seconds'.format(time()-t0),flush=True)
 
-for c_case in [
-        '"Caroline Ryden"',
-        '"Jane Knoche"',
-        '"Laura Edwards"',
-        '"Mark Williams"',
-        '"Sarah Kruger"',
+# Now do counselor specific cases for a handful of campuses
+for campus, names in [
+        ['Pritzker',['"Caroline Ryden"', '"Jane Knoche"',
+         '"Laura Edwards"', '"Mark Williams"', '"Sarah Kruger"']],
+        ['Golder',['"Edith Flores"','"Julie Horning"','"Lauren Chelew"']],
+        ['TNA',['Bermudez','Boros','Paiz','Weingartner','Wilson']],
+        ['UIC',['Ballard','Bowdy','Desgrosellier','MacCallum']],
         ]:
-    t0 = time()
-    print('Generating {}...'.format('Pritzker;'+c_case),flush=True,end='')
-    os.system(call_stem+' -q -ca Pritzker -co '+c_case)
-    print('{:.2f} seconds'.format(time()-t0),flush=True)
-
-for c_case in [
-        '"Edith Flores"',
-        '"Julie Horning"',
-        '"Lauren Chelew"',
-        ]:
-    t0 = time()
-    print('Generating {}...'.format('Golder;'+c_case),flush=True,end='')
-    os.system(call_stem+' -q -ca Golder -co '+c_case)
-    print('{:.2f} seconds'.format(time()-t0),flush=True)
-
-for c_case in [
-        'Bermudez',
-        'Boros',
-        'Paiz',
-        'Weingartner',
-        'Wilson',
-        ]:
-    t0 = time()
-    print('Generating {}...'.format('TNA;'+c_case),flush=True,end='')
-    os.system(call_stem+' -q -ca TNA -co '+c_case)
-    print('{:.2f} seconds'.format(time()-t0),flush=True)
-
-for c_case in [
-        'Ballard',
-        'Bowdy',
-        'Desgrosellier',
-        'MacCallum',
-        ]:
-    t0 = time()
-    print('Generating {}...'.format('UIC;'+c_case),flush=True,end='')
-    os.system(call_stem+' -q -ca UIC -co '+c_case)
-    print('{:.2f} seconds'.format(time()-t0),flush=True)
-
+    this_dir_before = os.listdir('.')
+    for name in names:
+        t0 = time()
+        print('Generating {}...'.format(campus+';'+name),flush=True,end='')
+        os.system(call_stem+' -q -ca '+campus+' -co '+name)
+        print('{:.2f} seconds'.format(time()-t0),flush=True)
+    this_dir_after = os.listdir('.')
+    new_files = [x for x in this_dir_after if x not in this_dir_before]
+    print(new_files)
+    folder = datetime.datetime.now().strftime(campus+
+            '_Counselor_Reports_%m_%d_%Y')
+    os.mkdir(folder)
+    for x in new_files:
+        os.rename(x, folder+'/'+x)
+    compress(folder)
