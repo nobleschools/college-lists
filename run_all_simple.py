1c906729dd9473f1056df0f49e6e83d3babe5ac9
Matt Niksch <mniksch@nobleschools.org>
2018-09-18 15:56:46 -0500

    simple updates

---
+++ run_all_simple.py
@@ -0,0 +1,36 @@
+#!python3
+"""Runs just the basic college lists"""
+from time import time
+import os
+
+call_stem = 'python create_reports.py'
+
+t0 = time()
+os.system(call_stem+' -sum Campus')
+print('Network: {:.2f} seconds'.format(time()-t0),flush=True)
+
+# Now do campus reports
+for campus_case in [
+        'Noble',
+        'Pritzker',
+        'Rauner',
+        'Golder',
+        'RoweClark',
+        'Comer',
+        'UIC',
+        'Bulls',
+        'Muchin',
+        'Johnson',
+        'DRW',
+        'Hansberry',
+        'Baker',
+        'Butler',
+        'Speer',
+        'TNA',
+        'PAS',
+        ]:
+    t0 = time()
+    print('Generating {}...'.format(campus_case),flush=True,end='')
+    os.system(call_stem+' -q -ca '+campus_case)
+    print('{:.2f} seconds'.format(time()-t0),flush=True)
+
